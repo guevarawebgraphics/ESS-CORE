50283259219f375c5d5506a092302c6b4530219e
Man <skkiyoshin@gmail.com>
2019-05-10 10:27:51 +0800

    Account Verification,AuthSecurity,AnnouncementNotification,BugFixes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-
+use Carbon\Carbon;
+use App\User;
 class LoginController extends Controller
 {
     /*
@@ -39,6 +40,73 @@ class LoginController extends Controller
     {
         //return $request->only($this->username(), 'password');
         return ['username' => $request->{$this->username()}, 'password' => $request->password, 'AccountStatus' => 1];
+    }
+    /**
+     * Authenticate the Expire User or Account the user login request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     */
+    protected function authenticated(\Illuminate\Http\Request $request)
+    {
+        $errors = [$this->username() => trans('auth.failed')];
+        // Load user from database
+        $user = User::where($this->username(), $request->{$this->username()})->first();
+
+        $updated_at = $user->updated_at;
+        $expiry_date = $user->expiry_date;
+        $password_expiry_at = Carbon::parse($updated_at)->addDays($expiry_date);
+        if($password_expiry_at->lessThan(Carbon::now())){
+            // Check if the user is not yet Verified
+            if($user->expiry_date == "14") {
+                $errors = [$this->username() => trans('auth.expired')];
+                auth()->logout();
+                return redirect('login')->withErrors($errors);
+            }
+        }
+        // return redirect()->back()
+        //             ->withInput($request->only($this->username(), 'remember'))
+        //             ->withErrors($errors);
+
+    }
+    /**
+     * Validate the user login request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     */
+    protected function validateLogin(\Illuminate\Http\Request $request)
+    {
+        $this->validate($request, [
+            $this->username() => 'required|exists:users',
+            'password' => 'required',
+        ],
+        [
+            $this->username() . '.exists' => 'The Username is Invalid.'
+        ]);
+    }
+    /**
+     * Get the failed login response instance.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function sendFailedLoginResponse(\Illuminate\Http\Request $request)
+    {
+        $errors = [$this->username() => trans('auth.failed')];
+
+        // Load user from database
+        $user = User::where($this->username(), $request->{$this->username()})->first();
+
+        // Check if user was successfully loaded, that the password matches
+        // and active is not 1. If so, override the default error message.
+        if ($user->AccountStatus != 1) {
+            $errors = [$this->username() => trans('auth.deactivated')];
+        }
+
+        return redirect()->back()
+            ->withInput($request->only($this->username(), 'remember'))
+            ->withErrors($errors);
     }
     /**
      * Create a new controller instance.
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,28 +1,64 @@
+// $(document).ready(function (){
+    showAllAnnouncementToNotification();
 
-// $(document).ready( function (){
-// 	var url = $('#CreateAccount').attr('action');
-// 	var data = $('#CreateAccount').serialize();
-// 	$('#test').click(function (e){
-// 		e.preventDefault();
-// 		$.ajaxSetup({
-// 			 headers: {
-// 					 'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
-// 			 }
-// 	 });
-// 	$.ajax({
-// 			type: 'ajax',
-// 			method: 'post',
-// 			url: '/Account',
-// 			data: {"shortname": shortname},
-// 			async: false,
-// 			dataType: 'json',
-// 			success: function (response) {
-// 					console.log("success");
-// 			},
-// 			error: function (response) {
-// 					console.log("Error cannot be");
-// 			}
-// 			});
-// 	});
+    $('#announcement').click(function (){
+        showAllAnnouncementToNotification();
+    });
+
+    $('#announcementdesc').on('click', '.show_announcement_notification',function (){
+        var title = $(this).attr('data-title');
+        var description = $(this).attr('data-description');
+        swal({
+
+                title: title,
+                text: description,
+                showCancelButton: true,
+            },
+
+
+
+        );
+    });
+    function showAllAnnouncementToNotification(){
+        // Show Notification
+        $.ajax({
+            type: 'ajax',
+            method: 'get',
+            url: '/Announcement/get_all_announcement_to_notification',
+            async: false,
+            dataType: 'json',
+            success: function (data) {
+                var html = '';
+                var i;
+                var count = 1;
+                for(i=0; i<data.length; i++){
+                    var status = (data[i].announcement_status == 1 ? 'Posted' : data[i].announcement_status == 0 ? 'Pending' : null);
+                    const date = new Date(data[i].updated_at);
+                    $('#notif').html(count++);
+                    html += '<a class="dropdown-item show_announcement_notification" href="#" id="Announcement_Notification"  data-title="'+data[i].announcement_title+'" data-description="'+data[i].announcement_description+'"><!-- Message Start -->'+
+                            '<div class="media">'+
+                            '<img alt="User Avatar" class="img-size-50 mr-3 img-circle" src="../dist/img/user3-128x128.jpg">'+
+                            '<div class="media-body">'+
+                            '<h3 class="dropdown-item-title">'+data[i].announcement_title+'<span class="float-right text-sm text-danger"><i class="fa fa-star"></i></span></h3>'+
+                            '<p class="text-sm">'+data[i].announcement_description+'</p>'+
+                            '<p class="text-sm text-muted"><i class="fa fa-clock-o mr-1"></i>'+date.toDateString()+'</p>'+
+                            '</div>'+
+                            '</div><!-- Message End --></a>'+
+                            '<div class="dropdown-divider"></div><a class="dropdown-item" href="#"><!-- Message Start -->';
+                }
+                if(status == 'Posted'){
+                    $('#announcementdesc').html(html);
+                    
+                }
+                else if(status == 'Pending'){
+                    $('#announcementdesc').html('No Announcement Found');
+                }
+                
+                //console.log("success");
+            },
+            error: function (response) {
+                
+            }
+        });
+    }
 // });
-
