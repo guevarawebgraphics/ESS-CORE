db0d31ff39228066c5f4be337ad50790df34731c
Justin Bunag <justinbunag1998@gmail.com>
2019-08-16 11:11:14 +0800

    ESS Bug Fixes

--- resources/views/Template/index.blade.php
+++ resources/views/Template/index.blade.php
@@ -155,7 +155,7 @@ elseif(Session::get('manage_docs') == 'delete'){
 <!-- Delete System Notification -->
 <div class="modal fade" id="DeleteTemplateModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
-          <div class="modal-content">
+          <div class="modal-content card-custom-blue card-outline">
             <div class="modal-header">
               <h5 class="modal-title" id="title_modal"></h5>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
